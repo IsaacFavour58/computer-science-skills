db61dee94551c8323955b8c931d32de37706c452
isaac favour <isaacfavour5858@gmail.com>
2023-01-31 05:47:38 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// JavaScript code for handling the form submission and validating the user's login information
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var username = document.querySelector("#username").value;
+    var password = document.querySelector("#password").value;
+    // Verify the user's login information against a database
+    // If the login is successful, redirect the user to the protected part of the website
+    // If the login is unsuccessful, display an error message
+  });
+  
